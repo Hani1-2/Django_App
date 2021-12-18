7d94912c47e95f1ade65a853c3d73584949d914c
Hani1-2 <63501992+Hani1-2@users.noreply.github.com>
2021-12-18 20:01:38 +0500

    update

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 from polls import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('services/', views.contact, name='contact'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
 
